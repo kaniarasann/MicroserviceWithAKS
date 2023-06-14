612bc19bcf42d3ba84210558b94e82512476c44f
kaniarasan nagalingam <kaniarasan@outlook.com>
2023-06-13 22:07:28 -0500

    Added docker

---
+++ ShoppingApp/ShoppingApp/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+
+namespace ShoppingApp.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
